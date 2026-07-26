f9c008578905fd532c988aac29f6ac2896a73ecc
Bruno Sousa <brunofsousa88@gmail.com>
2026-07-26 18:25:41 -0300

    Adiciona modelos e melhorias no ScreenMatch

--- screnmatch/screnmatch/src/main/java/br/com/alura/screnmatch/ScrenmatchApplication.java
+++ screnmatch/screnmatch/src/main/java/br/com/alura/screnmatch/ScrenmatchApplication.java
@@ -1,8 +1,6 @@
 package br.com.alura.screnmatch;
 
-import br.com.alura.screnmatch.model.DadosSerie;
-import br.com.alura.screnmatch.service.ConsumoApi;
-import br.com.alura.screnmatch.service.ConverteDados;
+import br.com.alura.screnmatch.principal.Principal;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -16,13 +14,12 @@ public class ScrenmatchApplication implements CommandLineRunner {
 
 	@Override
 	public void run(String... args) throws Exception {
-		var consumoApi = new ConsumoApi();
-		var json = consumoApi.obterDados("https://www.omdbapi.com/?t=gilmore+girls&apikey=eb5f6b63");
-//		System.out.println(json);
-//      json = consumoApi.obterDados("https://coffee.alexflipnote.dev/random.json");
-		System.out.println(json);
-		ConverteDados conversor = new ConverteDados();
-		DadosSerie dados = conversor.obterDados(json, DadosSerie.class);
-		System.out.println(dados);
+		Principal principal = new Principal();
+		principal.exibeMenu();
+
+
+
+
+
 	}
 }
